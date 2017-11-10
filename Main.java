2d5850729e9906567fefc9de206499883d53700c
bac5899 <blake@recordsone.com>
2017-11-10 10:22:57 -0500

    Stage 1/4, creating skeleton code. Next stage (after completion) is to place class pieces together.

---
+++ Main.java
@@ -0,0 +1,8 @@
+/**
+ * Main method class, used to instantiate whole fleet of programs/ classes.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
