8cfc7a42ff1ebbececfe2e94bb11b494ebb02e05
Gavin Drabik <grd2747@g.rit.edu>
2017-11-27 15:32:06 -0500

    Working copy. Exclude the Model for now
    Making changes to the view

--- View/View.java
+++ View/View.java
@@ -1,7 +1,12 @@
 package View;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
 
-import javax.swing.*;
-import java.awt.*;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -9,42 +14,35 @@ import java.util.Observer;
  * View class serves as the default template that holds all the functionality of the jpanels
  * individual components
  */
-public class View extends JFrame implements Observer{
-    //has instances of all the jpanels, as well as the model
-    JPanel createUserView = null;
-    JPanel homeView = null;
-    JPanel loginView = null;
-    JPanel projectView = null;
-    //now we need an instantion of the model
-    // TODO: 11/26/17 can't get the model instance in here -Blake
+public class View extends Application implements Observer{
 
-    /**
-     * default view constructor
-     */
-    public View() {
-        JFrame frame = new JFrame("View Demo"); //create new frame
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //create default close operation
+    @Override
+    public void update(Observable o, Object arg) {
 
-        // TODO: 11/26/17 component information goes here. need to make this -blake
-        //https://docs.oracle.com/javase/tutorial/uiswing/components/frame.html
-        frame.getContentPane().add(new Label("Hello there"),BorderLayout.CENTER);
-
-        //size frame
-        //frame.pack();
-        frame.setSize(500,500);
-        //center frame
-        frame.setLocationRelativeTo(null);
-        //show it
-        frame.setVisible(true);
     }
 
-    /**
-     * update method from Observer to update the view panel
-     * @param observable
-     * @param o
-     */
-    @Override
-    public void update(Observable observable, Object o) {
+    Button button;
 
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        //primaryStage is the window that will appear
+        primaryStage.setTitle("main window");
+        button = new Button("Button Text");
+
+        button.setOnAction(e -> {
+            System.out.println("lamda expression");
+            System.out.println("lamda expression2");
+        });//lambda action event
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout,300,250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
