b1bb5e354bdbf67a4ad44c713ce86bd9e7a3495a
Matthew Turczmanovicz <MattTurcz@gmail.com>
2017-11-29 21:59:16 -0500

    Added Insert, Update, and Delete sample statements to the Statement Script. - Matthew Turcz

--- DatabaseFiles/SQLFiles/Statement_Script.sql
+++ DatabaseFiles/SQLFiles/Statement_Script.sql
@@ -36,3 +36,64 @@ SELECT UserName, FirstName, LastName, EmailAddress, GraduationDate, Major, Name,
 SELECT UserName, FirstName, LastName, EmailAddress, OfficeNumber, PhoneNumber, user_committee_link.Role, Department, Name, Summary, Topic FROM user JOIN email USING (UserName) JOIN phone USING (UserName) JOIN office USING (UserName) JOIN user_committee_link USING (UserName) JOIN committee ON user_committee_link.CommitteID = committee.ID JOIN project ON committee.ProjectID = project.ID;
 
 SELECT project.Name, Summary, Topic, milestone.Name, statuses.Description, milestone.Number, milestone.DueDate, milestone.Approved FROM project JOIN project_milestone_link ON project.ID = project_milestone_link.ProjectID JOIN milestone ON milestone.ID = project_milestone_link.MilestoneID JOIN statuses ON statuses.Code = milestone.StatusCode;
+
+----------------------------
+-- Inserts
+----------------------------
+
+INSERT INTO committee (ID, ProjectID, ProjectGrade) VALUES (?, ?, ?);
+
+INSERT INTO email (UserName, EmailAddress, EmailType) VALUES (?, ?, ?);
+
+INSERT INTO milestone (ID, StatusCode, Name, Number, DueDate, Approved) VALUES (?, ?, ?, ?, ?, ?);
+
+INSERT INTO office (UserName, OfficeNumber) VALUES (?, ?);
+
+INSERT INTO phone (UserName, PhoneNumber, PhoneType) VALUES (?, ?);
+
+INSERT INTO project (ID, Name, Summary, Topic, Type, StartDate, EndDate, DueDate, Completed, ProposalApproved, FinalDefenseDate, PlagiarismPercentage, Grade, CommitteeID) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+
+INSERT INTO statuses (Code, Description) VALUES (?, ?);
+
+INSERT INTO user (UserName, FirstName, LastName, Password, Image, GraduationDate, Department, Major, Role) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);
+
+
+----------------------------
+-- Updates
+----------------------------
+
+UPDATE committee SET value = ? WHERE ID = ?;
+
+UPDATE email SET value = ? WHERE UserName = ?;
+
+UPDATE milestone SET value = ? WHERE ID = ?;
+
+UPDATE office SET value = ? WHERE UserName = ?;
+
+UPDATE phone SET value = ? WHERE UserName = ?;
+
+UPDATE project SET value = ? WHERE ID = ?;
+
+UPDATE statuses SET value = ? WHERE Code = ?;
+
+UPDATE user SET value = ? WHERE UserName = ?;
+
+----------------------------
+-- Deletes
+----------------------------
+
+DELETE FROM committee WHERE ID = ?;
+
+DELETE FROM email WHERE UserName = ?;
+
+DELETE FROM milestone WHERE ID = ?;
+
+DELETE FROM office WHERE UserName = ?;
+
+DELETE FROM phone WHERE UserName = ?;
+
+DELETE FROM project WHERE ID = ?;
+
+DELETE FROM statuses WHERE Code = ?;
+
+DELETE FROM user WHERE UserName = ?;
