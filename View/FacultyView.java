045b9e5332b074d838c3e40794a2ae5352274984
Oswaldo Rosete <oswaldorosete@oar3327.student.rit.edu>
2017-12-09 15:40:13 -0500

    updated FacultyView.java -- now has top menu

--- View/FacultyView.java
+++ View/FacultyView.java
@@ -1,5 +1,7 @@
 package View;
 
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -22,20 +24,48 @@ public class FacultyView {
 
     public FacultyView(){}
 
-    public Scene makeUserView() {
+    // TODO: 12/8/17 Document and fix indentation -Blake
+    public Scene makeUserView(){
+        BorderPane root = new BorderPane(); //Main layout is a border pane
+        Scene sc = new Scene(root,1366,768);//put main pane is a scene
+        sc.getStylesheets().add(getClass().getResource("Style.css").toExternalForm());//add css files
 
-      gp.setHgap( 5 );
-      gp.setVgap( 5 );
-      gp.setAlignment( Pos.CENTER );
+        HBox menuButtons = makeMenuButtons("User");//Use function to make the menu buttons. Decides which to show as clicked.
 
-      gp.add( new Label("Name: "), 0,0);
-      gp.add( new Label("User Name: "), 0,1);
-      gp.add( new Label("Department: "), 0,2);
-      gp.add( new Label("Role: "), 0,3);
-      gp.add( new Label("THIS ONE IS FACULTY: "), 0,0);
+        root.setTop(menuButtons);//Put menu buttons at top of page
+        root.setCenter(gp);//set center of page to grid pane (class attribute)
 
-      Scene sc = new Scene(gp,600,400);
+        gp.setHgap( 5 );
+        gp.setVgap( 5 );
+        gp.setAlignment( Pos.CENTER );
 
-      return sc;
-   }
+        gp.add( new Label("Name: "), 0,0);
+        gp.add( new Label("User Name: "), 0,1);
+        gp.add( new Label("Department: "), 0,2);
+        gp.add( new Label("Role: "), 0,3);
+        gp.add( new Label("THIS ONE IS FACULTY: "), 0,0);
+
+        //Scene sc = new Scene(gp,600,400);
+
+        return sc;
+    }
+    
+    public HBox makeMenuButtons(String viewName){
+        HBox returnMenu = new HBox();
+        Button userInfoButton = new Button("User Information");
+        Button projectInfoButton = new Button("Project Information");
+        Button logOutButton = new Button("Log Out");
+        returnMenu.getChildren().addAll(userInfoButton,projectInfoButton,logOutButton);
+ 
+        if (viewName.equals("User")){
+            System.out.println("user view");
+        }
+        if (viewName.equals("Project")){
+            System.out.println("Project view");
+
+
+        }
+
+        return returnMenu;
+    }
 }
