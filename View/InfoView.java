ca9ed8f87ef2c3e8828ccf64c9e5e3762804124a
Quinn <qsb2538@rit.edu>
2017-12-12 21:02:13 -0500

    Added code for facualty view generation. It is currently not displaying everything yet. Will try to get this fixed by 12-12-17

--- View/InfoView.java
+++ View/InfoView.java
@@ -169,9 +169,47 @@ public class InfoView extends Observable {
         //this scene object comes from masterView. it has a border pane in it. The top object of the border pane has been set to the nav bar
         //the borderpane can be referenced by casting an object seen below
         Scene sc = mv.getBaseScene();
+        //add the style sheet to the Scene
+        sc.getStylesheets().add(getClass().getResource("Style.css").toExternalForm());//add css files
         BorderPane bp = (BorderPane) sc.getRoot();
+        gp = new GridPane();
+        //add the grid pane to the border pane
+        bp.setCenter(gp);
 
-        //After the scene is made completely, these two methods run which will update the master view to our new view
+        //set margins between cells
+        gp.setHgap(5);
+        gp.setVgap(10);
+        //center the grid pane on the page
+        gp.setAlignment(Pos.CENTER);
+
+        //make the main header
+        userInfoHeaderLabel = new Label("Faculty Information");//Bigger header on top
+        //style the header with css
+        userInfoHeaderLabel.getStyleClass().add("mainHeader");
+        userInfoHeaderLabel.setAlignment(Pos.CENTER);
+
+        //add the header to the gird pane
+        gp.add(userInfoHeaderLabel, 0, 0, 2, 1);
+        labName = new Label("Name: ");
+        labName.getStyleClass().add("infoLabel");
+
+        labUserName = new Label("User Name: ");
+        labUserName.getStyleClass().add("infoLabel");
+
+        labDepartment = new Label("Department: ");
+        labDepartment.getStyleClass().add("infoLabel");
+
+        labRole = new Label("Role: ");
+        labRole.getStyleClass().add("infoLabel");
+
+        gp.add(labName, 0, 1);
+        gp.add(labUserName, 0, 2);
+        gp.add(labDepartment, 0, 3);
+        gp.add(labRole, 0, 6);
+        gp.gridLinesVisibleProperty().setValue(true);
+
+        loadStudentDBInfo();//this sets the labels to the proper info using the local class instance of MSDB
+        //loop through each of the user fields that we have and put them next to the label. They come in in the correct order
         setChanged();
         notifyObservers(sc);
     }
