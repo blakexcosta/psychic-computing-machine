6b39fb4ecb000f8a3bcc59a7ff379d67c1cd1c93
Student <student@howard01.istlabs.rit.edu>
2017-12-15 13:38:46 -0500

    MilestoneView.java: popup form autofills with current info

--- View/MilestoneView.java
+++ View/MilestoneView.java
@@ -102,9 +102,13 @@ public class MilestoneView extends Observable {
         Button editMilestoneButton = new Button("Edit Milestone");
 
         editMilestoneButton.setOnAction(e -> {
-            makeStudentEditMilestonePopup();
+            makeStudentEditMilestonePopup(_msID);
         });
         gp.addColumn(0, makeMilestoneDropdown(), statusLab, nameLab, numberLab, dueDateLab, approvedLab,editMilestoneButton);
+        
+        milestoneDropdown.setValue(rs.get(1).get(2).toString());
+        System.out.println("value should be changed: " + rs.get(1).get(2).toString());
+        
         for (String curr : rs.get(1)) {
             Label lab = new Label(curr);//make a new label with the DB text
             gp.add(lab, 1, rowCount);
@@ -114,7 +118,7 @@ public class MilestoneView extends Observable {
         this.currMSID = _msID;
     }
 
-    public void makeStudentEditMilestonePopup(){
+    public void makeStudentEditMilestonePopup(String _msID){
         Stage popupWindow = new Stage();
         gp = new GridPane();
         Scene popupInfo = new Scene(gp, 600, 800);
@@ -128,8 +132,11 @@ public class MilestoneView extends Observable {
         inputDate.setPromptText("YYYY-MM-DD");
 
         Button submitButton = new Button("Submit Changes");
-        ArrayList<String> milestoneIDAL = new ArrayList<>(Arrays.asList(currMSID));
+        ArrayList<String> milestoneIDAL = new ArrayList<>(Arrays.asList(_msID));
 
+        ArrayList<ArrayList<String>> rs = msdb.getData("SELECT * FROM milestone WHERE ID=(?) ", new ArrayList<String>(Arrays.asList(_msID)));
+        inputName.setText(rs.get(1).get(2));
+        inputDate.setText(rs.get(1).get(4));
         submitButton.setOnAction(e -> {
             if (!inputName.getText().isEmpty()) {
                 msdb.setData("UPDATE milestone set Name='" + inputName.getText() + "' where ID in (?)", milestoneIDAL);
@@ -137,6 +144,7 @@ public class MilestoneView extends Observable {
             if (!inputDate.getText().isEmpty()) {
                 msdb.setData("UPDATE milestone set DueDate='" + inputDate.getText() + "' where ID in (?)", milestoneIDAL);
             }
+            
             makeStudentView();
             popupWindow.close();
         });
