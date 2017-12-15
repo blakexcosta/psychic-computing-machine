1763dc8088ac3ca8c1144677b00a9be4baa4c105
Gavin Drabik <grd2747@g.rit.edu>
2017-12-15 13:42:47 -0500

    Only email to floeser or Gavin

--- View/ProjectView.java
+++ View/ProjectView.java
@@ -497,8 +497,14 @@ public class ProjectView extends Observable {
                     }
                     else{
                         //this is where it would be sent to...
-                        System.out.println("mv.sendEmail(" + rs.get(1).get(0) + "),'email Text'");
-                        mv.sendEmail("grd2747@rit.edu","test sending an email");
+                        //ONLY SEND EMAILS TO FLOESER
+                        if (rs.get(1).get(0).equals("Michael.Floeser@rit.edu")){
+                            mv.sendEmail(rs.get(1).get(0),"test sending an email");
+                        }
+                        else{
+                            System.out.println("mv.sendEmail(" + rs.get(1).get(0) + "),'email Text'");
+                            mv.sendEmail("grd2747@rit.edu","test sending an email");
+                        }
                     }
                 });
                 Label lab = new Label(curr.get(0));
